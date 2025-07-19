561281eaaa4eaf37bda7b6a606e7abdd02680f77
Lostpay <vincentiusshane@gmail.com>
2025-07-20 00:09:50 +0700

    prepared for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,20 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "../middleware/ratelimiter.js";
 import cors from "cors"
+import path from "path"
 dotenv.config();
 
 const app=express();
 const port=process.env.PORT;
-
+const __dirname=path.resolve()
 //middleware
-app.use(
-    cors({
-        origin:["http://localhost:5173"],
-    })
-)
+if(process.env.Node_ENV!== "production"){
+    app.use(
+        cors({
+            origin:["http://localhost:5173"],
+        })
+    )
+}
 app.use(express.json())
 app.use(rateLimiter)
 
@@ -25,6 +28,12 @@ app.use(rateLimiter)
 // })
 app.use("/api/notes",notesRoutes);
 
+if(process.env.Node_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))//combines the 2 ports of backend and front end into 1
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 connectDB().then(()=>{
     app.listen(port,() => {
         console.log("server started on PORT: ",port);
--- frontend/lib/axios.jsx
+++ frontend/lib/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+//dynamic url for production
+const BASE_URL=import .meta.env.MODE==="development" ? "http://localhost:5001/api":"/api"
 const axiosApi = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 export default axiosApi;
